bb8c9d3e0bdeda9792f2ffb3eaf0df239c4ba7b1
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2024-10-24 23:05:09 +0200

    Subida del Contador a drawer

--- Flutter1-SFR/Ej1/lib/screens/enlace5.dart
+++ Flutter1-SFR/Ej1/lib/screens/enlace5.dart
@@ -1,70 +1,94 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const Enlace5());
+}
+
 class Enlace5 extends StatelessWidget {
   const Enlace5({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Micontador(); // Aquí solo devuelves el widget que quieres mostrar
+    return MaterialApp(
+      title: 'Contador de Clicks',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
-class Micontador extends StatefulWidget {
-  const Micontador({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<Micontador> createState() => _MicontadorState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MicontadorState extends State<Micontador> {
-  int contador = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Samuel Fernández Rodríguez")),
+        title: const Text('Contador de Clicks'),
       ),
       body: Center(
-        child: Text("Has pulsado $contador veces"),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            onPressed: incrementar,
-            child: const Text("+"),
-          ),
-          FloatingActionButton(
-            onPressed: decrementar,
-            child: const Text("-"),
-          ),
-          FloatingActionButton(
-            onPressed: resetear,
-            child: const Text("Reset"),
-          ),
-        ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Samuel Fernandez Rodriguez',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              'Has hecho $_counter clicks',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text('Sumar'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  child: const Text('Restar'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _resetCounter,
+                  child: const Text('Resetear'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  void incrementar() {
-    setState(() {
-      contador++;
-    });
-  }
-
-  void decrementar() {
-    setState(() {
-      if (contador > 0) {
-        contador--;
-      }
-    });
-  }
-
-  void resetear() {
-    setState(() {
-      contador = 0;
-    });
   }
 }
