baf849c220c597294d008aea80bbe2e1b6da8da4
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2024-11-20 19:29:26 +0100

    Prueba de JavaFx proyecto MyGames

---
+++ MyGamesApp/src/main/java/controlers/VentanaPrincipalController.java
@@ -0,0 +1,10 @@
+package controlers;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+public class VentanaPrincipalController {
+
+    @FXML
+    private BorderPane PanelPrincipal;
+
+}
